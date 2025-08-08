98ee391a3d3f8fa84d483085a5ace8984c3e0c6c
pashi44 <pashireddi@gmail.com>
2025-08-08 18:28:22 -0500

    added reucer   with  exported post actoioo n statements

--- onpsqlview/src/App.jsx
+++ onpsqlview/src/App.jsx
@@ -1,61 +1,47 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TwitterPage from "./components/TwitterPage"; // adjust path as needed
+import TwitterPage from "./components/TwitterPage";
 import ZomatoComponent from './components/Zomato';
-
-import { useSelector, shallowEqual, useDispatch } from 'react-redux';
-
+import { setName } from './reduxstore/store';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  // Accessing values from store
+  const storeObjectName = useSelector((state) => state.Name);
+  const storeObjectDesc = useSelector((state) => state.Description);
 
+  const dispatch = useDispatch();
 
-
-const  storeObjectName =  useSelector((stateonj) => (stateonj.Name));
-const  storeObjectDesc =  useSelector((stateonj) =>(stateonj.Description));
-
-const  dispacthAction  =  useDispatch();
-const handleClick = (e)=> dispacthAction({type : "Get_state" , payload : e.target.value });
-
+  // Update store when input changes
+  const handleClick = (e) => dispatch(setName(e.target.value));
 
   return (
-<div>
-  
-<div  className="store">
-<div>
+    <div>
+      <div className="store">
+        <div>
+          <ul>
+            <li>{storeObjectName || ""}</li>
+            <li>{storeObjectDesc || ""}</li>
+          </ul>
 
-<ul>
-<li>{storeObjectName  || ""}</li>
+          <input
+            type='text'
+            onChange={(e) => handleClick(e)}
+          />
+        </div>
+      </div>
 
-<li>{storeObjectDesc || ""}</li>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/tweets" element={<TwitterPage />} />
+            <Route path="/records" element={<ZomatoComponent />} />
+          </Routes>
+        </Router>
+      </div>
 
-</ul>
-
-<input type='text'  
-onChange={
-
- (e) =>handleClick(e)
-
-} 
-/>
-
-
-</div>
-</div>
-<div>
-      <Router>
-      <Routes>
-        <Route path="/tweets" element={<TwitterPage />} />
-        <Route path="/records" element={<ZomatoComponent />} />
-
-
-      </Routes>
-    </Router>
-</div>
-
-{/* <p> {storeObject.Name} </p> */}
-
-</div>
-
+      {/* <p>{storeObject.Name}</p> */}
+    </div>
   );
 }
 
--- onpsqlview/src/reduxstore/store.jsx
+++ onpsqlview/src/reduxstore/store.jsx
@@ -1,37 +1,53 @@
-// import { legacy_createStore } from "redux";//  deperected and migrated to  configureStore
+// import { legacy_createStore } from "redux"; // deprecated and migrated to configureStore
+
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+// Initial state for the slice
+const initialState = {
+  Name: "prashanth",
+  Age: '',
+  Description: "this is object from redux store"
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
 
 
-
-import React, { act } from 'react';
-import  {configureStore}  from '@reduxjs/toolkit'
-const myobj =  {
-
-
-    Name : "prashanth",
-    Age :  '',
-    Description : "thisis object  from redux store"
-} 
-
-//limitataions of the the reducer functinis it should be synchronus funation and do not mutate the 
-// original state , rather  copy  a reference and append to re-render mouniting
-const  myreducerFucntion =  ( initialstate  =  myobj, action)=> 
-{
-switch(action.type){
-case  "Get_state":
-  return  {...initialstate ,Name : action.payload}
-
-
-    default: 
-    return   {...initialstate};
-
-}
-}
-
-const  store =  configureStore(
- {  reducer  : myreducerFucntion
-
+  reducers: {
+    setName(state, action) {
+      // Redux Toolkit uses Immer under the hood, so we can "mutate" here safely
+      state.Name = action.payload; 
+    },
+    setAge(state, action) {
+      state.Age = action.payload;
+    },
+    setDescription(state, action) {
+      state.Description = action.payload;
+    }
   }
+});
 
-);
+// Limitations of reducer functions:
+// 1. Reducers must be synchronous functions.
+// 2. Do not mutate the original state in classic Redux (must return a new object).
+//    BUT: Redux Toolkit uses Immer, so "mutations" are actually safe and become immutable updates under the hood.
+// Example of classic reducer:
+// const myReducerFunction = (initialState = myobj, action) => {
+//   switch (action.type) {
+//     case "Get_state":
+//       return { ...initialState, Name: action.payload };
+//     default: 
+//       return { ...initialState };
+//   }
+// };
 
-export default  store;
+// Configure the Redux store
+const store = configureStore({
+  reducer: userSlice.reducer // can also pass { user: userSlice.reducer } for multi-slice
+});
+
+// Export actions so components can dispatch them
+export const { setName, setAge, setDescription } = userSlice.actions;
+
+export default store;
